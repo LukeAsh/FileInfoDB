714487e637372133392c5df00f56bd9b30974ccd
lukasz.walach <lukasz@walach.net>
2016-05-29 23:35:56 +0300

    Almost ready for production

---
+++ __init__.py
@@ -0,0 +1,95 @@
+"""
+Traverse given directory and get list of all available files (full path)
+
+Currently checking WD Elements
+    '''
+    lvscan
+    lvchange -ay /dev/wde
+    for i in `ls -x1 /dev/wde`;do udisks --mount /dev/wde/$i; don
+    '''
+"""
+import csv
+import hashlib
+import logging as log
+import os
+
+CSV_DATABASE = 'file_hashes.csv'
+HASHES = ('md5', 'sha512')
+
+
+def get_all_files_from_dir(directory):
+    gen = os.walk(directory)
+    fn_list = []
+    for dirpath, dirnames, filenames in gen:
+        for fn in filenames:
+            path = os.path.abspath(os.path.join(dirpath, fn))
+            if os.path.isfile(path):
+                fn_list.append(path)
+        for dn in dirnames:
+            fn_list += get_all_files_from_dir(dn)
+    return fn_list
+
+
+def generate_hashes_from_file(filepath):
+    hashes = {h: None for h in HASHES}
+    try:
+        with open(filepath, 'rb') as f:
+            file_content = f.read()
+            for h_name in hashes.keys():
+                hashes[h_name] = getattr(hashlib, h_name)(file_content).hexdigest()
+    except PermissionError as e:
+        log.exception(e)
+    return hashes
+
+def save_to_csv(data, filename=CSV_DATABASE):
+    """
+    Write FileInfo data to csv file
+    :return:
+    """
+    log.info('data: {0}'.format(data))
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        for file_info in data:
+            writer.writerow(file_info.get_attributes())
+
+
+class FileInfo(object):
+    def __init__(self, path, hashes=None):
+        self.path = path
+        self.name = os.path.basename(path)
+        self.size = os.path.getsize(self.path)
+        self.ctime = os.path.getctime(self.path)
+        self.mtime = os.path.getmtime(self.path)
+        self.atime = os.path.getatime(self.path)
+        self.hashes = hashes
+
+    def get_attributes(self):
+        """
+        :return: output for csv writer
+        """
+        out = [
+            self.path,
+            self.name,
+            self.size,
+            self.ctime,
+            self.mtime,
+            self.atime,
+        ]
+        hashes = [self.hashes[h] for h in self.hashes]
+        return out + hashes
+
+
+def main():
+    file_info_list = []
+    f_paths = get_all_files_from_dir(r'/tmp')
+    for f_path in f_paths:
+        hashes = generate_hashes_from_file(f_path)
+        log.info('f: {filepath} hashes: {hashes}'.format(
+            filepath=f_path,
+            hashes=generate_hashes_from_file(f_path))
+        )
+        file_info_list.append(FileInfo(f_path, hashes=hashes))
+    save_to_csv(data=file_info_list)
+
+if __name__ == '__main__':
+    main()
